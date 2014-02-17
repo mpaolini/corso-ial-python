5309a5a1195711d8fd1f256a94170a7a0e83d48f
Marco Paolini <marco@credra.com>
2014-02-17 12:39:56 +0100

    Implement authentication in chat server.

--- lesson-4/server/chat/tests.py
+++ lesson-4/server/chat/tests.py
@@ -5,17 +5,65 @@ from chat.models import ChatMessage
 
 class MessageTest(TestCase):
 
-    def test_create(self):
+    def test_create_noauth(self):
         resp = self.client.post(
             '/message',
-            '{"text": "ciao", "username": "pippo"}',
+            '{"text": "ciao"}',
+            content_type='application/json')
+        self.assertEqual(resp.status_code, 401)
+
+    def test_read_noauth(self):
+        resp = self.client.get('/message')
+        self.assertEqual(resp.status_code, 401)
+
+    def test_create_wrong_pass(self):
+        from django.contrib.auth.models import User
+        user = User(username='pippo',
+                    email='pippo@example.com')
+        user.set_password('pass')
+        user.save()
+        resp = self.client.post(
+            '/message',
+            '{"text": "ciao"}',
+            HTTP_X_USERNAME='pippo',
+            HTTP_X_PASSWORD='xxx',
+            content_type='application/json')
+        self.assertEqual(resp.status_code, 401)
+
+    def test_create_wrong_user(self):
+        from django.contrib.auth.models import User
+        user = User(username='pippo',
+                    email='pippo@example.com')
+        user.set_password('pass')
+        user.save()
+        resp = self.client.post(
+            '/message',
+            '{"text": "ciao"}',
+            HTTP_X_USERNAME='pluto',
+            HTTP_X_PASSWORD='xxx',
+            content_type='application/json')
+        self.assertEqual(resp.status_code, 401)
+
+    def test_create(self):
+        from django.contrib.auth.models import User
+        user = User(username='pippo',
+                    email='pippo@example.com')
+        user.set_password('pass')
+        user.save()
+        resp = self.client.post(
+            '/message',
+            '{"text": "ciao"}',
+            HTTP_X_USERNAME='pippo',
+            HTTP_X_PASSWORD='pass',
             content_type='application/json')
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(
             ChatMessage.objects.count(),
             1)
-        # test message really exists
-        resp = self.client.get('/message')
+        # test message reallpy exists
+        resp = self.client.get('/message',
+                               HTTP_X_USERNAME='pippo',
+                               HTTP_X_PASSWORD='pass')
         self.assertEqual(resp.status_code, 200)
         messages = json.loads(resp.content)
         self.assertEqual(
--- lesson-4/server/chat/views.py
+++ lesson-4/server/chat/views.py
@@ -3,17 +3,33 @@ from django.http import HttpResponse
 from chat.models import ChatMessage
 import json
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
 
 
 @csrf_exempt
 def message(request):
+
+    try:
+        username = request.META['HTTP_X_USERNAME']
+        password = request.META['HTTP_X_PASSWORD']
+    except KeyError:
+        return HttpResponse(status=401)
+
+    try:
+        user = User.objects.get(username=username)
+    except User.DoesNotExist:
+        return HttpResponse(status=401)
+
+    if not user.check_password(password):
+        return HttpResponse(status=401)
+
     if request.method == 'POST':
         # fetch message body
         body = request.body
         message = json.loads(body)  # deserialize
         chat_message = ChatMessage(
             text=message['text'],
-            username=message['username'])
+            username=user.username)
         chat_message.save()
         return HttpResponse('')
     else:
